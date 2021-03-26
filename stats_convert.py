04342c907580bfbf694d974181142b7a9f0b8ab1
coolkingcole <coolkingcole@gmail.com>
2021-03-26 16:26:10 -0400

    Update stats_convert.py

--- stats_convert.py
+++ stats_convert.py
@@ -5,7 +5,30 @@ import pandas as pd
 #import geopandas#uncomment for pandas
 
 import matplotlib.pyplot as plt
-from scipy import stats
+#from scipy import stats
+
+###these will go in util later
+def arcprint(anything):
+    arcpy.AddMessage(type(anything))
+    arcpy.AddMessage(anything)
+
+def makePlot(ndarr,field):
+    plt.plot(fields_ndarr[field])
+    plt.show()
+
+def ndarr2Pd(ndarr,field):
+    df = pd.DataFrame(ndarr[field])
+    return df
+
+def describe1D_ARR(df):
+    arcprint(df_describe.describe())
+
+def correlate2D_ARR(df):
+    #arcpy.AddMessage(df_describe.describe())
+    pass
+
+
+
 
 arcpy.env.overwriteOutput = True
 
@@ -16,33 +39,43 @@ if map == None:
     arcpy.AddError("Error getting map with aprx.activeMap")
     exit()
 
-user_layer = arcpy.GetParameterAsText(0)#trt00_shp.shp
-multi_field = arcpy.GetParameter(1)
+user_layer = arcpy.GetParameter(0)#trt00_shp.shp
+#arcprint(user_layer)
+#multi_field = arcpy.GetParameter(1)
 
+"""
 for field in multi_field:
     pass
+"""
 
 layer = map.addDataFromPath(user_layer)
-fields_ndarr = arcpy.da.TableToNumPyArray(user_layer, "*", skip_nulls=True)
+fields_ndarr = arcpy.da.TableToNumPyArray(layer, "*", skip_nulls=True)
+fields_ndarr = arcpy.da.FeatureClassToNumPyArray(layer, ('*'))
+arcprint(fields_ndarr)
 #total = field[output_dissolve_SUM_POP_name].sum()
-
-arcpy.AddMessage(type(fields_ndarr))#<class 'numpy.ndarray'>
-arcpy.AddMessage(type(user_layer))
-arcpy.AddMessage(type(layer))
-arcpy.AddMessage(fields_ndarr.dtype.names)
-
+"""
+arcprint(fields_ndarr)#<class 'numpy.ndarray'>
+arcprint(user_layer)
+arcprint(layer)
+arcprint(fields_ndarr.dtype.names)
+"""
 #this is a good way to get a pandas dataframe
 #gdf = geopandas.read_file(user_layer)
-#arcpy.AddMessage(type(gdf))
+#arcpy.AddMessageQ(type(gdf))
 #rho = np.corrcoef(fields_ndarr['Area'])
 
 # Make the first plot 
-plt.plot(fields_ndarr['Area'])#, fields_ndarr['TRACTID'])
+#, fields_ndarr['TRACTID'])
 
 
 # Show the figure. 
-#plt.show()
 
+#makePlot(fields_ndarr,'EnrollTota')
 
-df_describe = pd.DataFrame(fields_ndarr['Area'])
-arcpy.AddMessage(df_describe.describe())
+#df_describe = ndarr2Pd(fields_ndarr,'AApct')
+#describe1D_ARR(df_describe)
+df = pd.DataFrame(fields_ndarr['ASpct'])
+arcprint(df)
+#arcprint(df.describe())
+
+makePlot(fields_ndarr,'EnrollTota')
