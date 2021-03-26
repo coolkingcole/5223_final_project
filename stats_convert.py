d0bca079dce40b649338302f1aa49bd2c4927edb
coolkingcole <coolkingcole@gmail.com>
2021-03-26 17:43:25 -0400

    Update stats_convert.py

--- stats_convert.py
+++ stats_convert.py
@@ -21,7 +21,7 @@ def ndarr2Pd(ndarr,field):
     return df
 
 def describe1D_ARR(df):
-    arcprint(df_describe.describe())
+    arcprint(df.describe())
 
 def correlate2D_ARR(df):
     #arcpy.AddMessage(df_describe.describe())
@@ -29,17 +29,16 @@ def correlate2D_ARR(df):
 
 
 arcpy.env.overwriteOutput = True
-
 aprx = arcpy.mp.ArcGISProject("CURRENT")
-
 map = aprx.activeMap
 if map == None:
     arcpy.AddError("Error getting map with aprx.activeMap")
     exit()
 
+#Params
 user_layer = arcpy.GetParameter(0)
-fieldOfIntrest = arcpy.GetParameter(1)
-arcprint(fieldOfIntrest)
+fieldsOfIntrest = arcpy.GetParameter(1)
+arcprint(fieldsOfIntrest[0])
 makePlotBool = arcpy.GetParameter(2)
 #arcprint(user_layer)
 #multi_field = arcpy.GetParameter(1)
@@ -50,33 +49,25 @@ for field in multi_field:
 """
 
 layer = map.addDataFromPath(user_layer)
-fields_ndarr = arcpy.da.TableToNumPyArray(layer, "*", skip_nulls=True)
-fields_ndarr = arcpy.da.FeatureClassToNumPyArray(layer, ('*'))
-arcprint(fields_ndarr)
-#total = field[output_dissolve_SUM_POP_name].sum()
-"""
-arcprint(fields_ndarr)#<class 'numpy.ndarray'>
-arcprint(user_layer)
-arcprint(layer)
-arcprint(fields_ndarr.dtype.names)
-"""
-#this is a good way to get a pandas dataframe
-#gdf = geopandas.read_file(user_layer)
-#arcpy.AddMessageQ(type(gdf))
-#rho = np.corrcoef(fields_ndarr['Area'])
+#fields_ndarr = arcpy.da.TableToNumPyArray(layer, "*", skip_nulls=True)
+fields_ndarr = arcpy.da.FeatureClassToNumPyArray(layer, ('*'))#<class 'numpy.ndarray'>
 
-# Make the first plot 
-#, fields_ndarr['TRACTID'])
+if len(fieldsOfIntrest) == 1:
+    #arcprint(fields_ndarr.dtype.names)
+    #this is a good way to get a pandas dataframe
+    #gdf = geopandas.read_file(user_layer)
+    #arcpy.AddMessageQ(type(gdf))
+    #rho = np.corrcoef(fields_ndarr['Area'])
+    df = pd.DataFrame(fields_ndarr[str(fieldsOfIntrest[0])])
+    #arcprint(df)
+    #arcprint(df.describe())
+    describe1D_ARR(df)
+    
+    if makePlotBool:
+        makePlot(fields_ndarr,str(fieldsOfIntrest[0]))
 
+elif len(fieldsOfIntrest) == 2:
+    arcprint("2 fields hit")
 
-# Show the figure. 
-
-#makePlot(fields_ndarr,'EnrollTota')
-
-#df_describe = ndarr2Pd(fields_ndarr,'AApct')
-#describe1D_ARR(df_describe)
-df = pd.DataFrame(fields_ndarr['ASpct'])
-arcprint(df)
-#arcprint(df.describe())
-if makePlotBool:
-    makePlot(fields_ndarr,'EnrollTota')
+elif len(fieldsOfIntrest) > 2:
+    arcprint("greater than 2 fields hit")
